f4e35b7352c0901dfc3343660f6bf3acf8237928
thirdbot <astroll656@gmail.com>
2025-11-08 20:48:36 +0700

    changes

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -7,6 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to React with TypeScript and AWS CDK! CI/CD Pipeline Setup Example
+        </h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
